30c78c2438ce1f1836c8d667123b9cb987aeadc1
alexfelipe00 <56352969+alexfelipe00@users.noreply.github.com>
2024-04-05 08:25:24 -0400

    Estudo nodejs

---
+++ fundamentos/hello.js
@@ -0,0 +1,6 @@
+//Nodemon - Atualiza as alterações do código em tempo real
+//npm run dev - Executa o script "dev" dentro do package.json
+//Importa o pacote colors
+const colors = require('colors')
+
+console.log("Hello World".red)
