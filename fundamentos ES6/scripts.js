e166fc81d5d4ffeafd7630a6a74471a3fc6c626a
unknown <alexfelipe00@gmail.com>
2024-09-27 11:02:25 -0400

    .

--- fundamentos ES6/scripts.js
+++ fundamentos ES6/scripts.js
@@ -1,4 +1,4 @@
-// 1 - var, let e const
+// 1 - VAR E LET
 //Variavel sendo atribuida de forma global
 var x = 10
 var y = 15
@@ -22,9 +22,72 @@ if (b > 10){
     console.log(a)
 }
 
-console.log(a)
 
-//Const não é possivel reatribuir valores
-const a = 10
 
-console.log(a)
+
+// 2 - CONST
+//Const também é uma variavel de bloco e é uma variavel que não pode ser reatribuida com novos valores
+function logName(){
+    const name = 'Matheus'
+    console.log(name)
+}
+
+const name = 'Pedro'
+
+logName()
+
+console.log(name)
+
+
+
+// 3 - ARROW FUNCTION
+//Função comum
+const sum = function sum(a, b){
+    return a + b
+}
+console.log(sum(5,5))
+
+
+//Arrow function com corpo
+const arrowSum = (a, b) => {
+    return a + b
+}
+
+const greeting = (name) => {
+    if(name){
+        return 'Olá ' + name
+    }else{
+        return 'Olá'
+    }
+}
+console.log(greeting('Alex'))
+
+
+
+//Arrow function sem corpo resumida nome função 
+const sumArrowFunctionSummary = (a, b) => a + b
+
+
+console.log(arrowSum(5,5))
+
+//Arrow function sem argumentos
+const testeArrow = () => console.log("Testou!")
+
+testeArrow()
+
+
+//This ArrowFunction
+const user = {
+    name: "Theo "
+}
+
+//4 - FILTER
+const arr = [1, 2, 3, 4, 5]
+// .filter percorre o array aplicando a função
+const highNumber = arr.filter((n) => {
+    if (n>=3){
+        return n
+    }
+})
+
+console.log(highNumber)
