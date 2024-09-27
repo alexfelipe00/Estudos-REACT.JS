42d9babaa987fbdd79ffd381c5c5cf1ab10c3980
unknown <alexfelipe00@gmail.com>
2024-09-27 16:54:03 -0400

    .

--- fundamentos ES6/scripts.js
+++ fundamentos ES6/scripts.js
@@ -83,7 +83,7 @@ const user = {
 
 //4 - FILTER
 const arr = [1, 2, 3, 4, 5]
-// .filter percorre o array aplicando a função
+// .filter percorre o array aplicando a função, utilizado para fazer filtros
 const highNumber = arr.filter((n) => {
     if (n>=3){
         return n
@@ -91,3 +91,77 @@ const highNumber = arr.filter((n) => {
 })
 
 console.log(highNumber)
+
+
+
+const users = [
+    {name: 'Jô', available: false}, 
+    {name: 'Antonio', available: false}, 
+    {name: 'Marcio', available: true},
+    {name: 'João', available: false},
+    {name: 'Matheus', available: true}
+]
+
+//Retorna valores com true na coluna available
+const availableUsers = users.filter((user) => user.available)
+console.log(availableUsers)
+
+//Retorna valores com valor false na coluna available
+const notAvailableUsers = users.filter((user) => !user.available)
+console.log(notAvailableUsers)
+
+
+
+//5 - MAP
+//Método que percorre todos os elementos para modificar os valores aplicando uma função em cada elemento
+
+const products = [
+    {name: 'Camisa', price: 10.99, category: 'Roupas'},
+    {name: 'Chaleira Eletrica', price: 49.99, category: 'Eletro'},
+    {name: 'Fogão', price: 999, category: 'Eletro'},
+    {name: 'Calça', price: 80.99, category: 'Roupas'},
+    {name: 'Meia', price: 15.09, category: 'Roupas'}
+]
+
+products.map((product) => {
+    if (product.category === 'Roupas'){
+        product.onSale = true
+    }
+})
+
+console.log(products)
+
+
+
+//6 - TEMPLATE LITERALS
+const username = 'Matheus'
+const age = 28
+
+//Utilizando literals
+console.log(`O usuário ${username} possuí ${age} anos.`)
+
+//Utilizando concatenação (método antigo)
+console.log("O usuário "+ username +" possuí "+ age + " anos.")
+
+
+//7 - Desctructuring
+//Utilizado para transformar valores da lista são atribuidos à variaveis
+//Utilizando valor
+const fruits = ['Maçã', 'Laranja', 'Mamão']
+
+const [f1, f2, f3] = fruits
+
+console.log(f1)
+
+//Utilizando em objetos
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Eletronico",
+    color: "Cinza"
+}
+
+//Possui renomear variavel que ira receber os valores
+const {name: productName, price, category: productCategory, color} = productDetails
+
+console.log(`O nome do produto é ${productName} \nR$${price}\nCategoria: ${productCategory}\nCOR: ${color}`)
