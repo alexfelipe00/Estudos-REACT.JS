4a2ead362298c11413b8b4418bce22a5070a9075
unknown <alexfelipe00@gmail.com>
2024-10-10 13:46:07 -0400

    .

--- 3_AVANCANDO_EM_REACT/desafio3/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/desafio3/src/components/ConditionalRender.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 const ConditionalRender = () => {
+  const [x] = useState(false)
+
+
+  const [name] = useState("João")
+
   return (
     <div>
-        
+         <h1>Isso será exibido?</h1>
+         {x && <p>Se x for true, sim!</p>}
+         {!x && <p>Agora x é falso</p>}
+         {name}
     </div>
   )
 }
