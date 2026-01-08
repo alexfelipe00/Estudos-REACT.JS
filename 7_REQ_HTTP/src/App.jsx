aa21575aa3ae8ec893f39772b52e009ea337cba4
alexfelipe00 <alexfelipe00@gmail.com>
2026-01-08 16:37:51 -0400

    .

--- 7_REQ_HTTP/src/App.jsx
+++ 7_REQ_HTTP/src/App.jsx
@@ -5,6 +5,8 @@ const url = "http://localhost:3001/products"
 
 function App() {
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
 
 // 1 - resgatando dados
@@ -27,6 +29,12 @@ function App() {
 
   console.log(products)
 
+// 2 - adição produtos
+
+  const handleSubmit = async (e) =>{
+
+  }
+
   return (
       <div className="App">
         <h1>Lista de Produtos</h1>
@@ -35,6 +43,19 @@ function App() {
           <li key={product.id}>{product.name} - {product.price}R$</li>
           )}
         </ul>
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name}  name="name" onChange={(e) => setName(e.target.value)}/>
+            </label>
+            <label>
+              Preço:
+              <input type="number" value={price}  name="price" onChange={(e) => setPrice(e.target.value)}/>
+            <input type="submit" value="Criar"/>
+            </label>
+          </form>
+        </div>
       </div>
   )
 }
